8244d830d13e3e3dd6dbfe3b2b229bac548624de
svyat935 <svyat935@mail.ru>
2021-03-20 20:14:04 +0300

    Added connect to database and docker-compose draft

--- ponpon_core/main.py
+++ ponpon_core/main.py
@@ -1,4 +1,11 @@
+from database.connect import engine, Base
 from fastapi import FastAPI
+
+TABLE_TO_CREATE = (
+    # Base.metadata.tables["users"]
+)
+
+Base.metadata.create_all(bind=engine, tables=TABLE_TO_CREATE)
 
 app = FastAPI()
 
